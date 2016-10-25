c56f7cd1ecae739de688e2b3e3dc881509a69078
Unknown <smithi@stlukesct.org>
2016-10-25 10:03:27 -0400

    find

--- find.py
+++ find.py
@@ -1,14 +1,16 @@
 #for a given path group and explore it for frees and return a list of addresses of frees
 def find(path_group,cfg):
     addrFree=funcAddr("plt.free",cfg)
+    addrs=[]
     while len(path_group.active)>0:
         path_group.step()
         for path in path_group.active:
-		    if str(path.targets[-1]) == "<BV64 "+str(hex(addrFree)).rstrip("L")+">":
-                add=path.addr_trace[-1]
-                print add
-
+          if str(path.targets[-1]) == "<BV64 "+str(hex(addrFree)).rstrip("L")+">":
+            add=path.addr_trace[-1]
+            print hex(add)
+            addrs.append(add)
+    return addrs
 def funcAddr(name,cfg):
-	for addr,func in cfg.kb.functions.iteritems():
-		if func.name == name:
-			return addr
+    for addr,func in cfg.kb.functions.iteritems():
+        if func.name == name:
+            return addr
