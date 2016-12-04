80e20da95ff19f52af25043d5bd4e9603965e820
Unknown <smithi@stlukesct.org>
2016-12-04 14:01:09 -0500

    did some ddg stuff
    idk

--- use.py
+++ use.py
@@ -3,8 +3,9 @@ import networkx as nx
 import matplotlib.pyplot as plt
 def use(path_group, ddg,cfg):
     nx.draw(ddg.graph)
-    #for n in ddg.graph.nodes():
-    #if n.ins_addr==path_group.found[0].addr_trace
-    print hex(path_group.found[0].addr_trace[-1])
+    for n in ddg.graph.nodes():
+        if n.ins_addr == path_group.found[0].addr_trace[-1]:
+            print "here";
+    #print hex(path_group.found[0].addr_trace[-1])
     #print hex(path_group.found[0].ins_addr)
     plt.show()
